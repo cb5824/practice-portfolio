2c502e103c88a6d09db63af2dc075b7c3321125c
CB5824 <craig.baylis@gmail.com>
2019-10-20 23:45:04 +0000

    updated styling and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Project.create(title: "Portfolio Site", url: "https://github.com/cb5824/practice-portfolio", description: "This portfolio site you are currently viewing.")
-Project.create(title: "Prepr", url: "https://github.com/cb5824/Prepr", description: "A meal planning site I made while learning at Launch Academy")
+Project.find_or_create_by(title: "Portfolio Site", url: "https://github.com/cb5824/practice-portfolio", description: "This portfolio site you are currently viewing.")
+Project.find_or_create_by(title: "Prepr", url: "https://github.com/cb5824/Prepr", description: "A meal planning site I made while learning at Launch Academy")
+Project.find_or_create_by(title: "Test1", url: "https://github.com/cb5824", description: "Placeholder 1 for projects while finishing portfolio page. ")
+Project.find_or_create_by(title: "Test2", url: "https://github.com/cb5824", description: "Placeholder 2 for projects while finishing portfolio page. ")
+Project.find_or_create_by(title: "Test3", url: "https://github.com/cb5824", description: "Placeholder 3 for projects while finishing portfolio page. ")
+Project.find_or_create_by(title: "Test4", url: "https://github.com/cb5824", description: "Placeholder 4 for projects while finishing portfolio page. ")
